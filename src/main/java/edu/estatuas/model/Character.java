4297c7ba096a4d7c9f9cda1286c97ad79185e2e0
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2025-10-04 19:17:16 +0200

    feat(model): Add properties on Character

--- src/main/java/edu/estatuas/model/Character.java
+++ src/main/java/edu/estatuas/model/Character.java
@@ -1,15 +1,59 @@
 package edu.estatuas.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Character {
     @JsonProperty("id")
-    private final Integer id;
+    private Integer id;
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("status")
+    private String status;
+    @JsonProperty("image")
+    private String image;
+    @JsonProperty("gender")
+    private String gender;
+    @JsonProperty("origin")
+    private JsonNode origin;
 
-    public Character(Integer id) {
-        this.id = id;
-    }
+    public Character() {}
+
     public Integer getId() {
         return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public JsonNode getOrigin() {
+        return origin;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getName()).append(": {\n")
+            .append("ID: ").append(getId()).append("\n")
+            .append("Status: ").append(getStatus()).append("\n")
+            .append("Image: ").append(getImage()).append("\n")
+            .append("Gender: ").append(getGender()).append("\n}");
+        
+        return sb.toString();
     }
 }
